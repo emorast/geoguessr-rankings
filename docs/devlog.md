7ac6138011e0418091a3e8ed76a57c8f0715e87c
emorast <egil.morast@gmail.com>
2023-04-30 15:45:59 +0200

    Updated web-scraping functionality

--- docs/devlog.md
+++ docs/devlog.md
@@ -1,12 +1,16 @@
 # geoguessr-elo Devlog
+### 2023/03/30
+- Solved a login problem. Instead of giving `requests.session().post()` the *payload* as an dict, a `.json`is given instead.
+- Unfortunely called the API too many times. Looking into if it would be possible to pickle the session information (cookies etc.) and unpickle it instead of making another POST-request every tine the code runs.
+- Started new `Django` project.
 
 ### 2023/04/29
 - Working with the mockup for the application and figuring out what functionality would be implemented in the first version. To its core, the *geoguesr-elo* application have to be able to
     - scrape results from [GeoGuessr](https://www.geoguessr.com/).
-    - store the results in a database
+    - store the results in a db
     - calculate the contestants elo-rating
     - display a high-score-board
 
-- Decided that the frontend will be built on **React** and the backend will be built using **Django**.
+- Decided that the frontend will be built on `React`and the backend will be built using `Django`.
 
-- Added script to scrape the pro-league url and login-handler
+- Added script to scrape the pro-league url.
--- scripts/web-scraping.py
+++ scripts/web-scraping.py
@@ -1,28 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 from config import *
+import pickle
 
-def get_data_from_url(url):
-    req = requests.get(url)
-    return BeautifulSoup(req.content, 'html.parser')
+def save_session(session):
+    with open('save.pkl', 'wb') as f:
+        pickle.dump(session, f)
 
-def login_handler(data):
-    
-    tag = data.find_all('a', class_="button_link__xHa3x button_variantPrimary__xc8Hp")
-    login_url = url + str(tag[0].get('href'))
-
-def parse_data(data):
-    
-    leg_list = data.find_all('div', class_="leg-list__leg")
-    if len(leg_list) != 0:
-        for leg in leg_list:
-            print(leg)
+def load_session():
+    with open('save.pkl', 'rb') as f:
+        return pickle.load(f)
             
-    else:
-        login_handler(data)
-        
+def get_leg_results(data):
+    legs = data.find_all('div', class_ = "leg-list__leg")
+    if len(legs) !=0:
+        for leg in legs:
+            print(leg)
+
 if __name__ == '__main__':
-    
-    data = get_data_from_url(league_url)
-    
-    parse_data(data)
+    with requests.session() as session:
+        if login_required:
+            res = session.post(login_url, json={
+                "email": email,
+                "password": password},
+                headers=headers)
+            if res.status_code == 200:
+                print("Successful login")
+                save_session(session=session)
+            else:
+                print("Error: Unable to connect")           
+        else:
+            session = load_session()
+                
+        req = session.get(league_url)
+        data = BeautifulSoup(req.content, 'html.parser')
+        
+        get_leg_results(data)
+        
+        
+        
+        
+
