bb869fdd58e266b2d7de492f0d1e0828b9960e55
emorast <egil.morast@gmail.com>
2023-05-06 23:12:17 +0200

    Updated ranking-list

--- project/homeapp/views.py
+++ project/homeapp/views.py
@@ -7,7 +7,17 @@ TEMPLATE_DIRS = 'os.path.join(BASE_DIR, "templates").'
 
 def index(request):
     context = {}
+    players = Players.objects.order_by("-overall_elo")[:10]
+    context["players"] = players
+    context["showFullList"] = False
+
+    return render(request, "index.html", context)
+
+
+def list(request):
+    context = {}
     players = Players.objects.all().order_by("-overall_elo")
     context["players"] = players
+    context["showFullList"] = True
 
     return render(request, "index.html", context)
